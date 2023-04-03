81d28b770316c992e13d145e9faf02b8bcc9dd92
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2023-04-03 20:31:28 +0200

    test

--- src/views/pages/OrdersPage.vue
+++ src/views/pages/OrdersPage.vue
@@ -6,6 +6,8 @@ import Footer from '../utils/Footer.vue';
 import { useWebMiddleware } from '../../middlewares/webMiddleware';
 import { getOrdersApi } from '../../api/getOrdersApi';
 import OrdersGrid from '../components/OrdersPage/OrdersGrid.vue';
+import { useTranslationsStore } from '../../store/translations.store';
+
 
 const route = useRoute();
 const chatId = route.params.chat;
@@ -14,6 +16,7 @@ validateClient(chatId);
 const { orders, loadingOrders, getOrders } = getOrdersApi();
 getOrders(chatId);
 window.scrollTo(0, 0);
+const { trans } = useTranslationsStore();
 </script>
 
 <template>
@@ -21,6 +24,9 @@ window.scrollTo(0, 0);
     <Spinner v-if="loadingOrders" />
     <div v-else class="content">
         <div v-if="orders.length > 0">
+            <div class="header_title">
+                {{ orders.length }} {{ trans('d6e0081d-a4b9-4946-b873-1fbe05f8b282') }}
+            </div>
             <OrdersGrid :orders="orders" />
         </div>
     </div>
@@ -28,6 +34,11 @@ window.scrollTo(0, 0);
 </template>
 
 <style scoped>
+.header_title {
+    color: #848383;
+    font-size: 20px;
+    padding-bottom: 15px;
+}
 .content{
     padding: 20px 20px;
 }
