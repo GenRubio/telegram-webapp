ed5a31b36a4e3919f1d06665478bba9ab24ca24b
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2023-05-20 10:59:06 +0200

    test

---
+++ src/store/last-views.store.js
@@ -0,0 +1,30 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useLastViewsStore = defineStore("useLastViews", () => {
+  const lastViewProducts = ref([]);
+
+  const addLastViewProduct = (product) => {
+    lastViewProducts.value.forEach(function (lastViewProduct, index) {
+      if (lastViewProduct.reference == product.reference) {
+        lastViewProducts.value.splice(index, 1);
+      }
+    });
+    lastViewProducts.value.push(product);
+  };
+
+  const getLastViewedProducts = (actualProduct) => {
+    let lastTwoViewedProducts = [];
+    lastViewProducts.value.forEach(function (lastViewProduct, index) {
+      if (lastViewProduct.reference != actualProduct.reference) {
+        lastTwoViewedProducts.push(lastViewProduct);
+      }
+    });
+    return lastTwoViewedProducts.slice(-2);
+  };
+
+  return {
+    addLastViewProduct,
+    getLastViewedProducts,
+  };
+});
