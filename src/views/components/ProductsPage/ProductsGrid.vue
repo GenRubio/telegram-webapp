fe53f2a17ea2f95168993258446a570012400af6
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2023-05-22 06:47:42 +0200

    test

--- src/views/components/ProductsPage/ProductsGrid.vue
+++ src/views/components/ProductsPage/ProductsGrid.vue
@@ -1,30 +1,29 @@
 <script setup>
-import ProductsGridItem from './ProductsGridItem.vue';
-import { useGoRoute } from '../../../router/goRoute';
+import ProductsGridItem from "./ProductsGridItem.vue";
+import { useGoRoute } from "../../../router/goRoute";
 
-const props = defineProps([
-    "chatId",
-    "products",
-]);
+const props = defineProps(["chatId", "products"]);
 
 const clickProductHandle = (reference) => {
-    const { goProductDetailPage } = useGoRoute();
-    goProductDetailPage(props.chatId, reference);
-}
-
+  const { goProductDetailPage } = useGoRoute();
+  goProductDetailPage(props.chatId, reference);
+};
 </script>
 
 <template>
-    <div class="products-grid_container">
-        <ProductsGridItem v-for="product in products" :product="product"
-            @click="clickProductHandle(product.reference)" />
-    </div>
+  <div class="products-grid_container">
+    <ProductsGridItem
+      v-for="product in products"
+      :product="product"
+      @click="clickProductHandle(product.reference)"
+    />
+  </div>
 </template>
 
 <style scoped>
 .products-grid_container {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    gap: 5px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 5px;
 }
 </style>
